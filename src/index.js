1a15df6ab1d1e645a6e1adc0c85692504bbfcf93
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-06-23 19:59:14 +0200

    Refactored the application into smaller components and application will now work correctly (rendering details of a country or showing a not found message if there is an error response)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import ReactDOM from 'react-dom'
 import App from './App'
 
+const useField = (type) => {
+	const [value, setValue] = useState('')
+
+	const onChange = (event) => {
+		setValue(event.target.value)
+	}
+
+	return {
+		type,
+		value,
+		onChange,
+	}
+}
+
+const useCountry = (name) => {
+	const [country, setCountry] = useState(null)
+	const [error, setError] = useState(null)
+
+	useEffect(() => {
+		const getCountryDetails = async () => {
+			try {
+				const response = await axios.get(
+					`https://restcountries.eu/rest/v2/name/${name}?fullText=true`
+				)
+
+				console.log(response)
+
+				setCountry(response.data[0])
+			} catch (error) {
+				setError(error.response.data.message)
+			}
+		}
+
+		// Fetch details if name field is submitted
+		getCountryDetails()
+	}, [name])
+
+	return { country, error }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
+
+export { useField, useCountry }
